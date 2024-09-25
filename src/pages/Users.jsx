df1c76e1422f87e61db080982b4f6146db14fbed
Mukhiddin06 <muhiddinbegiboyev@gmail.com>
2024-09-25 14:53:38 +0500

    Added modal

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,13 +1,27 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import Button from '../components/Button'
 import Trash from '../assets/images/trash.svg'
 import More from '../assets/images/more.svg'
 import { useNavigate } from 'react-router-dom'
 import { Context } from '../context/Context'
+import Modal from '../components/Modal'
 
 function Users() {
+  const [showModal, setShowModal] = useState(false)
+  const [deleteId, setDeleteId] = useState(null)
+
   const navigate = useNavigate()
   const { users, deleteUser } = useContext(Context)
+
+  function handleDelete(id) {
+    setDeleteId(id)
+    setShowModal(true)
+  }
+  function handleDeleteBtn() {
+    deleteUser(deleteId)
+    setShowModal(false)
+  }
+
 
   return (
     <div>
@@ -36,7 +50,7 @@ function Users() {
                 <td className='text-center p-2'>{item.age}</td>
                 <td className='text-center p-2'>{item.address}</td>
                 <td className='text-center p-2 space-x-4'>
-                  <button onClick={() => deleteUser(item.id)} className='hover:scale-150 duration-300'><img src={Trash} alt="Delete" width={15} height={15} /></button>
+                  <button onClick={() => handleDelete(item.id)} className='hover:scale-150 duration-300'><img src={Trash} alt="Delete" width={15} height={15} /></button>
                   <button onClick={() => navigate(`/users/${item.id}`)} className='hover:scale-150 duration-300'><img src={More} alt="More" width={15} height={15} /></button>
                 </td>
               </tr>
@@ -44,6 +58,13 @@ function Users() {
           </tbody>
         </table>
       </div>
+      <Modal showModal={showModal} setShowModal={setShowModal}>
+        <p className="text-center mt-5 font-semibold text-gray-700">Are you sure you want to delete this user?</p>
+        <div className='flex justify-evenly space-x-4 mt-10'>
+          <button onClick={() => setShowModal(false)} className='px-4 py-2 bg-gray-200 text-gray-700 rounded-lg hover:bg-gray-300 transition duration-300'>Cancel</button>
+          <button onClick={handleDeleteBtn} className='px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600 transition duration-300'>Delete</button>
+        </div>
+      </Modal>
     </div>
   )
 }
